6f4980680ce0487be54753261c6262039c819783
Luis Ferrer Garrido <101939296+lferrerfmv@users.noreply.github.com>
2022-04-27 14:52:38 +0200

    Update 40-verified-files.sh

--- 40-verified-files.sh
+++ 40-verified-files.sh
@@ -6,4 +6,8 @@ if [ ! -f "/getssl/getssl" ]; then
     cp -r /tmp_getssl/getssl/* /getssl/
 fi
 
+if [ ! -f "/etc/nginx/conf.d/default.conf" ]; then
+    cp -r /etc/nginx/conf.d.tmp /etc/nginx/conf.d
+fi
+
 exit 0
